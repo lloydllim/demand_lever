ab1b7335fab7291efdff545674d08ccc2ee27fae
ohexa <kcxyz.xyz@gmail.com>
2025-01-10 07:17:51 +0800

    add Prisma module for dependency injection

---
+++ src/di/modules/create-prisma-module.ts
@@ -0,0 +1,11 @@
+import { DI_SYMBOLS } from "@/di/types";
+import { PrismaService } from "@/lib/prisma/infrastructure/services/prisma.service";
+import { createModule } from "@evyweb/ioctopus";
+
+export const createPrismaModule = () => {
+  const prismaModule = createModule();
+
+  prismaModule.bind(DI_SYMBOLS.IPrismaService).toClass(PrismaService);
+
+  return prismaModule;
+};
