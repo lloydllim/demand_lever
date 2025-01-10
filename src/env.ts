d310fc36f81c8011f056297179426914b6dddb03
ohexa <kcxyz.xyz@gmail.com>
2025-01-11 04:25:54 +0800

    feat: add Stripe environment variables for product and price IDs

--- src/env.ts
+++ src/env.ts
@@ -9,6 +9,15 @@ const envSchema = z.object({
   JWT_TOKEN: z.string(),
   AUTH_TOKEN: z.string(),
   NODE_ENV: z.string(),
+
+  STRIPE_SECRET_KEY: z.string(),
+  STRIPE_SDR_PRODUCT_ID: z.string(),
+  STRIPE_SDR_PRICE_ID: z.string(),
+  STRIPE_SDR_MANAGER_PRODUCT_ID: z.string(),
+  STRIPE_SDR_MANAGER_PRICE_ID: z.string(),
+  STRIPE_DATA_PACKAGE_PRODUCT_ID: z.string(),
+  STRIPE_DATA_PACKAGE_PRICE_1_ID: z.string(),
+  STRIPE_DATA_PACKAGE_PRICE_2_ID: z.string(),
 });
 
 const env = envSchema.parse(process.env);
