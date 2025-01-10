84b32967dc430f6a3d2c97b516bf7431fee8f9e5
ohexa <kcxyz.xyz@gmail.com>
2025-01-10 16:14:10 +0800

    feat(AuthService): extend IAuthService interface with token creation, password hashing, and signup methods

--- src/lib/auth/application/services/auth.service.interface.ts
+++ src/lib/auth/application/services/auth.service.interface.ts
@@ -1,5 +1,18 @@
+import { IPostUserModel } from "@/lib/user/entities/user.model";
 import { JwtPayload } from "jsonwebtoken";
 
 export interface IAuthService {
   verifyToken(token: string): string | JwtPayload;
+
+  createToken(
+    user_id: string,
+    user_name: string,
+    user_type: string,
+    expiresIn: string
+  ): string;
+
+  hashPassword(password: string): Promise<string>;
+  comparePassword(password: string, hashedPassword: string): Promise<boolean>;
+
+  signup(user: IPostUserModel): Promise<string>;
 }
