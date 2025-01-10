4e644354dd4c0962c2d553957e85f43f3ad42526
ohexa <kcxyz.xyz@gmail.com>
2025-01-10 17:26:00 +0800

    feat(di): enhance Prisma module binding with instrumentation and crash reporter services

--- src/di/modules/create-prisma-module.ts
+++ src/di/modules/create-prisma-module.ts
@@ -5,7 +5,12 @@ import { createModule } from "@evyweb/ioctopus";
 export const createPrismaModule = () => {
   const prismaModule = createModule();
 
-  prismaModule.bind(DI_SYMBOLS.IPrismaService).toClass(PrismaService);
+  prismaModule
+    .bind(DI_SYMBOLS.IPrismaService)
+    .toClass(PrismaService, [
+      DI_SYMBOLS.IInstrumentationService,
+      DI_SYMBOLS.ICrashReporterService,
+    ]);
 
   return prismaModule;
 };
