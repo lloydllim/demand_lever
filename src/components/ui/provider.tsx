783387642f43068ba7cd14a10c77c9eded6d0544
ohexa <kcxyz.xyz@gmail.com>
2025-01-10 21:18:52 +0800

    replace ColorModeProvider with ThemeProvider for improved theming support

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { ColorModeProvider } from "./color-mode";
+import { ThemeProvider } from "next-themes";
 
 export function Provider(props: Readonly<{ children: React.ReactNode }>) {
   return (
     <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
+      <ThemeProvider
+        defaultTheme="light"
+        attribute="class"
+        disableTransitionOnChange
+      >
+        {props.children}
+      </ThemeProvider>
     </ChakraProvider>
   );
 }
