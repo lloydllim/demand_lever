d9217075e10fa7824bb7d655ca542ff127020071
ohexa <kcxyz.xyz@gmail.com>
2025-01-10 17:25:32 +0800

    feat(Auth): store session token in cookies after user signup

--- src/app/actions/auth-post-signup.action.ts
+++ src/app/actions/auth-post-signup.action.ts
@@ -1,7 +1,11 @@
+"use server";
+
 import { getInjection } from "@/di/container";
 import { IPostUserModel } from "@/lib/user/entities/user.model";
+import { cookies } from "next/headers";
 
-export const authPostSignupAction = (input: IPostUserModel) => {
+export const authPostSignupAction = async (input: IPostUserModel) => {
+  const cookieStore = await cookies();
   const instrumentationService = getInjection("IInstrumentationService");
 
   return instrumentationService.instrumentServerAction(
@@ -10,7 +14,12 @@ export const authPostSignupAction = (input: IPostUserModel) => {
     async () => {
       const authSignupUseCase = getInjection("IAuthSignupUseCase");
       const token = await authSignupUseCase(input);
-      return token;
+
+      cookieStore.set("session", token, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 60 * 60 * 24, // 1 day
+      });
     }
   );
 };
