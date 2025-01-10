2c630468a84777c506294e5fd0104ef3c3535ffd
ohexa <kcxyz.xyz@gmail.com>
2025-01-10 16:14:02 +0800

    feat(AuthService): implement signup and token management with password hashing and instrumentation

--- src/lib/auth/infrastructure/services/auth.service.ts
+++ src/lib/auth/infrastructure/services/auth.service.ts
@@ -1,11 +1,15 @@
 import env from "@/env";
 import { IAuthService } from "@/lib/auth/application/services/auth.service.interface";
 import { IInstrumentationService } from "@/lib/instrumentation/application/services/instrumentation.service.interface";
+import { IUserRepository } from "@/lib/user/application/repositories/user.repository.interface";
+import { IPostUserModel } from "@/lib/user/entities/user.model";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export const AuthService = class implements IAuthService {
   constructor(
-    private readonly instrumentationService: IInstrumentationService
+    private readonly instrumentationService: IInstrumentationService,
+    private readonly userRepository: IUserRepository
   ) {}
 
   verifyToken(token: string) {
@@ -13,6 +17,64 @@ export const AuthService = class implements IAuthService {
       { name: "AuthService.verifyToken" },
       () => {
         return jwt.verify(token, env.JWT_TOKEN);
+      }
+    );
+  }
+
+  createToken(
+    user_id: string,
+    user_name: string,
+    user_type: string,
+    expiresIn: string
+  ): string {
+    return this.instrumentationService.startSpan(
+      { name: "AuthService.createToken" },
+      () => {
+        return jwt.sign({ user_id, user_name, user_type }, env.JWT_TOKEN, {
+          expiresIn,
+        });
+      }
+    );
+  }
+
+  async signup(user: IPostUserModel): Promise<string> {
+    return this.instrumentationService.startSpan(
+      { name: "AuthService.signup" },
+      async () => {
+        const createdUser = await this.userRepository.create(user);
+
+        // todo fix dirty no enum check
+        if (!createdUser.userType) {
+          throw new Error("User type is required");
+        }
+
+        return this.createToken(
+          createdUser.id,
+          createdUser.name,
+          createdUser.userType,
+          "1h"
+        );
+      }
+    );
+  }
+
+  async comparePassword(
+    password: string,
+    hashedPassword: string
+  ): Promise<boolean> {
+    return this.instrumentationService.startSpan(
+      { name: "AuthService.comparePassword" },
+      async () => {
+        return await bcrypt.compare(password, hashedPassword);
+      }
+    );
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    return this.instrumentationService.startSpan(
+      { name: "AuthService.hashPassword" },
+      async () => {
+        return await bcrypt.hash(password, 10);
       }
     );
   }
