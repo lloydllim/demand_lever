a1efa289ce3c241d0f2ec9a4f3dd1bcb77679967
ohexa <kcxyz.xyz@gmail.com>
2025-01-10 09:17:59 +0800

    add authentication services and symbols to dependency injection types

--- src/di/types.ts
+++ src/di/types.ts
@@ -1,17 +1,9 @@
+import { IAuthService } from "@/lib/auth/application/services/auth.service.interface";
+import { IAuthVerifyTokenUseCase } from "@/lib/auth/application/use-cases/auth-verify-token.use-case";
+import { IPostAuthVerifyTokenController } from "@/lib/auth/network-adapters/controller/post-auth-verify-token.controller";
 import { ICrashReporterService } from "@/lib/crash-reporter/application/services/crash-reporter.service.interface";
 import { IInstrumentationService } from "@/lib/instrumentation/application/services/instrumentation.service.interface";
 import { IPrismaService } from "@/lib/prisma/application/services/prisma.service.interface";
-
-export const DI_SYMBOLS = {
-  // instrumentation
-  IInstrumentationService: Symbol.for("IInstrumentationService"),
-
-  // crash-reporter
-  ICrashReporterService: Symbol.for("ICrashReporterService"),
-
-  // prisma
-  IPrismaService: Symbol.for("IPrismaService"),
-};
 
 export interface DI_RETURN_TYPES {
   // instrumentation
@@ -22,4 +14,25 @@ export interface DI_RETURN_TYPES {
 
   // prisma
   IPrismaService: IPrismaService;
+
+  // auth
+  IAuthService: IAuthService;
+  IAuthVerifyTokenUseCase: IAuthVerifyTokenUseCase;
+  IPostAuthVerifyTokenController: IPostAuthVerifyTokenController;
 }
+
+export const DI_SYMBOLS = {
+  // instrumentation
+  IInstrumentationService: Symbol.for("IInstrumentationService"),
+
+  // crash-reporter
+  ICrashReporterService: Symbol.for("ICrashReporterService"),
+
+  // prisma
+  IPrismaService: Symbol.for("IPrismaService"),
+
+  // auth
+  IAuthService: Symbol.for("IAuthService"),
+  IAuthVerifyTokenUseCase: Symbol.for("IAuthVerifyTokenUseCase"),
+  IPostAuthVerifyTokenController: Symbol.for("IPostAuthVerifyTokenController"),
+};
