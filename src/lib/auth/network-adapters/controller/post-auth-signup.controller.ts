1caf8579535545f2a71dcafa1bbdd9217727c612
ohexa <kcxyz.xyz@gmail.com>
2025-01-10 16:13:58 +0800

    feat(PostAuthSignupController): implement controller for user signup with input validation and instrumentation

---
+++ src/lib/auth/network-adapters/controller/post-auth-signup.controller.ts
@@ -0,0 +1,45 @@
+import { IAuthWSignupUseCase } from "@/lib/auth/application/use-cases/auth-signup.use-case";
+import { InputParseError } from "@/lib/common/entities/controller.error";
+import { IInstrumentationService } from "@/lib/instrumentation/application/services/instrumentation.service.interface";
+import { PostUserModel } from "@/lib/user/entities/user.model";
+import { z } from "zod";
+
+const presenter = (
+  instrumentationService: IInstrumentationService,
+  token: string
+) => {
+  return instrumentationService.startSpan({ name: "presenter" }, () => {
+    return {
+      token,
+    };
+  });
+};
+
+const inputData = PostUserModel;
+
+export type IPostAuthSignupController = ReturnType<
+  typeof PostAuthSignupController
+>;
+
+export const PostAuthSignupController =
+  (
+    instrumentationService: IInstrumentationService,
+    authSignupUseCase: IAuthWSignupUseCase
+  ) =>
+  (input: z.infer<typeof inputData>) => {
+    return instrumentationService.startSpan(
+      { name: "PostAuthSignupController" },
+      async () => {
+        const { data, error: inputParseError } = inputData.safeParse(input);
+
+        if (inputParseError) {
+          throw new InputParseError("Invalid input", {
+            cause: inputParseError.errors,
+          });
+        }
+
+        const token = await authSignupUseCase(input);
+        return presenter(instrumentationService, token);
+      }
+    );
+  };
