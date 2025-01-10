858b0594e7ae57141d66ea393289c5a8ff0e8d3b
ohexa <kcxyz.xyz@gmail.com>
2025-01-10 08:42:24 +0800

    add development error handling in CrashReporterService

--- src/lib/crash-reporter/infrastructure/services/crash-reporter.service.ts
+++ src/lib/crash-reporter/infrastructure/services/crash-reporter.service.ts
@@ -1,8 +1,13 @@
+import env from "@/env";
 import { ICrashReporterService } from "@/lib/shared/application/services/crash-reporter.service.interface";
 import * as Sentry from "@sentry/nextjs";
 
 export const CrashReporterService = class implements ICrashReporterService {
   report(error: unknown): string {
+    if (env.NODE_ENV === "development") {
+      throw error;
+    }
+
     return Sentry.captureException(error);
   }
 };
