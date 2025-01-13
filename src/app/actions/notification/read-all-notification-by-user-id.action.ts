67f0a249e64fd0895c5aa5780c4c174c4a8ead4e
ohexa <kcxyz.xyz@gmail.com>
2025-01-13 09:11:07 +0800

    feat: enhance notification actions with error handling and add update action for marking notifications as read

--- src/app/actions/notification/read-all-notification-by-user-id.action.ts
+++ src/app/actions/notification/read-all-notification-by-user-id.action.ts
@@ -1,24 +1,49 @@
-import { IToken } from "@/app/actions/auth/utils/token.type";
+"use server";
+
 import { cookieGetSessionOrJwt } from "@/app/actions/utils/cookie-get-session-or-jwt";
 import { getInjection } from "@/di/container";
+import { UnauthenticatedError } from "@/lib/auth/entities/auth.error";
+import { InputParseError } from "@/lib/common/entities/controller.error";
 
-export const readAllNotificationByUserIdAction = () => {
+export const readAllNotificationByUserIdAction = async () => {
   const instrumentationService = getInjection("IInstrumentationService");
   return instrumentationService.instrumentServerAction(
     "ReadAllNotificationByUserIdAction",
     { recordResponse: true },
     async () => {
       const session = await cookieGetSessionOrJwt();
+      try {
+        const readAllNotificationByUserIdController = getInjection(
+          "IReadAllNotificationByUserIdController"
+        );
 
-      if (!session) {
-        throw new Error("Unauthorized");
+        const notifications = await readAllNotificationByUserIdController({
+          token: session,
+        });
+
+        return {
+          data: notifications,
+        };
+      } catch (error) {
+        if (error instanceof UnauthenticatedError) {
+          return {
+            error: "Unauthenticated. Not signed in.",
+          };
+        }
+
+        if (error instanceof InputParseError) {
+          return {
+            error: "Failed to get notifications.",
+          };
+        }
+
+        const crashReporterService = getInjection("ICrashReporterService");
+        crashReporterService.report(error);
+        return {
+          error:
+            "An error happened while fetching notifications. The developers has been notified. Please try again later.",
+        };
       }
-
-      const userId = (session as unknown as IToken).user_id as string;
-      const readAllNotificationByUserIdController = getInjection(
-        "IReadAllNotificationByUserIdController"
-      );
-      return await readAllNotificationByUserIdController({ userId });
     }
   );
 };
