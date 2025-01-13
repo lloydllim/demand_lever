45c822c096e35771fef4751d02645fa68fbdcd6d
ohexa <kcxyz.xyz@gmail.com>
2025-01-13 09:11:38 +0800

    feat: implement ReadUserByIdAsClient action and remove deprecated userFindByIdAsClient action

---
+++ src/app/actions/user/read-user-by-id-as-client.action.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { cookieGetSessionOrJwt } from "@/app/actions/utils/cookie-get-session-or-jwt";
+import { getInjection } from "@/di/container";
+import { UnauthenticatedError } from "@/lib/auth/entities/auth.error";
+
+export const ReadUserByIdAsClientAction = async () => {
+  const instrumentationService = getInjection("IInstrumentationService");
+  return instrumentationService.instrumentServerAction(
+    "ReadUserByIdAsClientAction",
+    { recordResponse: true },
+    async () => {
+      try {
+        const session = await cookieGetSessionOrJwt();
+
+        if (!session) {
+          throw new UnauthenticatedError("Not authenticated");
+        }
+
+        const readUserByIdAsClientController = getInjection(
+          "IReadUserByIdAsClientController"
+        );
+
+        const response = await readUserByIdAsClientController({
+          token: session,
+        });
+
+        return {
+          result: response,
+        };
+      } catch (error) {
+        if (error instanceof UnauthenticatedError) {
+          return {
+            error: "Unauthenticated. Sign in to continue.",
+          };
+        }
+
+        const crashReporterService = getInjection("ICrashReporterService");
+        crashReporterService.report(error);
+        return {
+          error:
+            "An error occurred. The team has been notified. Please try again later.",
+        };
+      }
+    }
+  );
+};
